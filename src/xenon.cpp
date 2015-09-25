6ae3c095c26c22dede71a40a51585836323845fd
TAK2004 <thomas@ptscientists.com>
2015-09-25 07:50:20 +0200

    -added brotli library -added brotli compression to the output streams

--- src/xenon.cpp
+++ src/xenon.cpp
@@ -1,5 +1,6 @@
 #include "Xenon/xenon.hpp"
 #include "Xenon/dds.hpp"
+#include <encode.h>
 
 namespace Xenon {
 
@@ -82,6 +83,40 @@ void DeInterleave(RF_Collect::Array<RF_Mem::AutoPointerArray<RF_Type::UInt8> >& 
     });
 }
 
+void DyadicDecomposition(RF_Collect::Array<RF_Mem::AutoPointerArray<RF_Type::UInt8> >& Buffers,
+                  RF_Type::Size Elements)
+{
+    struct Channel
+    {
+        void Init(RF_Type::UInt8* Data, RF_Type::Size Blocks)
+        {
+            data = Data;
+            blocks = Blocks;
+        }
+        RF_Type::UInt8* data; RF_Type::Size blocks;
+    };
+
+    RF_Collect::Array<Channel> channels(10);
+    channels(0).Init(Buffers(2).Get(), Elements);
+    channels(1).Init(Buffers(3).Get(), Elements);
+    channels(2).Init(Buffers(4).Get(), Elements);
+    channels(3).Init(Buffers(5).Get(), Elements);
+    channels(4).Init(Buffers(6).Get(), Elements);
+    channels(5).Init(Buffers(7).Get(), Elements);
+    channels(6).Init(Buffers(0).Get(), Elements);
+    channels(7).Init(Buffers(1).Get(), Elements);
+    channels(8).Init(Buffers(10).Get(), Elements);
+    channels(9).Init(Buffers(11).Get(), Elements);
+
+    RF_Algo::ForEach(channels, [](RF_Collect::Array<Channel>::EnumeratorType& Enum) {
+        RF_Type::UInt8* blockIn = Enum->data;
+        for(RF_Type::Size i = 0; i < Enum->blocks; ++i)
+        {
+            blockIn[i] = blockIn[i];
+        }
+    });
+}
+
 void DDSProcessor::Process()
 {
     RF_Collect::Array<RF_Mem::AutoPointerArray<RF_Type::UInt8> > buffers(12);
@@ -102,7 +137,60 @@ void DDSProcessor::Process()
 
     DeInterleave_DXT5(ddsData, blocks, buffers);
     DeInterleave(buffers, blocks);
+    DyadicDecomposition(buffers, blocks);
+    
+    RF_Type::Size size = sizeof(DDS_HEADER);
+    brotli::BrotliParams param;
 
+    auto r0 = RF_Mem::AutoPointerArray<RF_Type::UInt8>(blocks);
+    RF_Type::Size bufSize= r0.Count();    
+    if (brotli::BrotliCompressBuffer(param, buffers(2).Count(), buffers(2).Get(), &bufSize, r0.Get()))
+        size += bufSize;
+    
+    auto g0 = RF_Mem::AutoPointerArray<RF_Type::UInt8>(blocks);
+    bufSize = g0.Count();
+    if(brotli::BrotliCompressBuffer(param, buffers(3).Count(), buffers(3).Get(), &bufSize, g0.Get()))
+        size += bufSize;
+
+    auto b0 = RF_Mem::AutoPointerArray<RF_Type::UInt8>(blocks);
+    bufSize = b0.Count();
+    if(brotli::BrotliCompressBuffer(param, buffers(4).Count(), buffers(4).Get(), &bufSize, b0.Get()))
+        size += bufSize;
+
+    auto r1 = RF_Mem::AutoPointerArray<RF_Type::UInt8>(blocks);
+    bufSize = r1.Count();
+    if(brotli::BrotliCompressBuffer(param, buffers(5).Count(), buffers(5).Get(), &bufSize, r1.Get()))
+        size += bufSize;
+
+    auto g1 = RF_Mem::AutoPointerArray<RF_Type::UInt8>(blocks);
+    bufSize = g1.Count();
+    if(brotli::BrotliCompressBuffer(param, buffers(6).Count(), buffers(6).Get(), &bufSize, g1.Get()))
+        size += bufSize;
+
+    auto b1 = RF_Mem::AutoPointerArray<RF_Type::UInt8>(blocks);
+    bufSize = b1.Count();
+    if(brotli::BrotliCompressBuffer(param, buffers(7).Count(), buffers(7).Get(), &bufSize, b1.Get()))
+        size += bufSize;
+
+    auto a0 = RF_Mem::AutoPointerArray<RF_Type::UInt8>(blocks);
+    bufSize = a0.Count();
+    if(brotli::BrotliCompressBuffer(param, buffers(0).Count(), buffers(0).Get(), &bufSize, a0.Get()))
+        size += bufSize;
+
+    auto a1 = RF_Mem::AutoPointerArray<RF_Type::UInt8>(blocks);
+    bufSize = a1.Count();
+    if(brotli::BrotliCompressBuffer(param, buffers(1).Count(), buffers(1).Get(), &bufSize, a1.Get()))
+        size += bufSize;
+
+    auto cbits = RF_Mem::AutoPointerArray<RF_Type::UInt8>(sizeof(RF_Type::UInt32)*blocks);
+    bufSize = cbits.Count();
+    if(brotli::BrotliCompressBuffer(param, buffers(10).Count(), buffers(10).Get(), &bufSize, cbits.Get()))
+        size += bufSize;
+
+    auto abits = RF_Mem::AutoPointerArray<RF_Type::UInt8>(sizeof(AlphaIndices)*blocks);
+    bufSize = abits.Count();
+    if(brotli::BrotliCompressBuffer(param, buffers(11).Count(), buffers(11).Get(), &bufSize, abits.Get()))
+        size += bufSize;
 
     m_Output = RF_Mem::AutoPointerArray<RF_Type::UInt8>(sizeof(DDS_HEADER));
     RF_SysMem::Copy(m_Output.Get(), ddsHeader, sizeof(ddsHeader));
